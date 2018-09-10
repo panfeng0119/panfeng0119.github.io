4fd8dba66d4465af511f0e0493fa8593083eae65
panfeng <312887499@qq.com>
2018-09-10 23:40:05 +0800

    add build.sh

--- hugo.sh
+++ hugo.sh
@@ -1,10 +1,13 @@
 # 脚本：自动将public文件夹push到master分支
 work_path=`pwd`
-echo "当前目录：${work_path}"
-echo "正在生成网站."
+Date=`date "+%Y-%m-%d %H:%M:%S"`
+alias log='echo ${Date} INFO'
+log "当前目录：${work_path}"
+# begin
+log "正在生成网站."
 hugo -b http://panfeng0119.github.io
 
-echo "克隆master分支."
+log "克隆master分支."
 rm -rf master
 git clone https://github.com/panfeng0119/panfeng0119.github.io.git master -b master
 
@@ -12,9 +15,9 @@ cd master
 git rm -rf .
 cp -rf ../public/* ./
 git add -A
-git commit -m"site update"
-echo "正在push."
+git commit -m"site update ${Date}"
+log "正在push."
 git push -u origin master
-echo Down!
+log "Down!"
 cd ${work_path}
 rm -rf master
