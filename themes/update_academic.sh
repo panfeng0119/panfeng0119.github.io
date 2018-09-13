33ecb5c32130c660a9eea6e10560807bfbf090bb
panfeng <312887499@qq.com>
2018-09-14 01:50:54 +0800

    学习配置3

--- themes/update_academic.sh
+++ themes/update_academic.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Display available updates to Academic.
-cd themes/academic
+cd academic
 git fetch
 git log --pretty=oneline --abbrev-commit --decorate HEAD..origin/master
 cd ../../
